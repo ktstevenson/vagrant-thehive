6efa2481e75084310cd695f33dff2d27b0efcc24
Katherine Talia Stevenson <ktstevenson@gmail.com>
2021-06-03 20:56:30 -0400

    Clarify which build is complete.

--- provisioning/thehive/scripts/thehive-02.sh
+++ provisioning/thehive/scripts/thehive-02.sh
@@ -5,5 +5,5 @@ chmod 640 /etc/thehive/application.conf
 systemctl enable thehive
 systemctl start thehive
 rmdir /var/tmp/provision
-echo "Build complete."
+echo "TheHive build complete."
 echo "Wait a minute or two and point a browser at http://localhost:9000"
